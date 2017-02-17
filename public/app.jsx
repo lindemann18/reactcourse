316f22a35926caa0d031206c9346e3725cb9cbec
lindemann18 <ashernetz@hotmail.com>
2017-02-17 12:14:28 -0700

    updated

---
+++ public/app.jsx
@@ -0,0 +1,36 @@
+ var Greeter = React.createClass({
+	getDefaultProps:function(){
+		return{
+			name: "react",
+			lastname:".js",
+			message:"nothing"
+		};
+	},
+	onButtonClick:function(e){
+		e.preventDefault();
+		var name = this.refs.name.value;
+		alert(name);
+	},
+	render: function(){
+		var name = this.props.name;
+		return(
+			<div>
+				<h1>Hello {this.props.name} {this.props.lastname}</h1>
+				<p>{this.props.message}</p>
+
+				<form onSubmit={this.onButtonClick}>
+					<input type="text" ref="name"/>
+					<button>Set Name</button>
+				</form>
+			</div>
+		);
+	}
+ });
+
+var name = "Chuvakovich";
+var lastname = "Zukovsky";
+var message = "holis vato";
+ ReactDOM.render(
+    <Greeter name={name} lastname={lastname} message = {message}/>,
+    document.getElementById("app")
+);
