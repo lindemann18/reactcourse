937c3e58df5e5165ca99a97a05bb94b32c6090ab
lindemann18 <ashernetz@hotmail.com>
2017-02-27 19:49:33 -0700

    added more styles

--- reactweather/app/components/Examples.jsx
+++ reactweather/app/components/Examples.jsx
@@ -1,8 +1,23 @@
-var React = require("react");
+var React  = require("react");
+var {Link} = require("react-router");
 
 var Examples = (props)=>{
 	return(
-			<h3>Examples Component</h3>
+			<div>
+				<h1 className="text-center">Examples Component</h1>
+				<p className="text-center">Here are a few examples locations to try out</p>
+				<ol className="text-center">
+					<li className="text-center">
+						<Link to="/?location=Culiacan">Culiacan </Link>
+					</li>
+					<li className="text-center">
+						<Link to="/?location=Mochis">Mochis </Link>
+					</li>
+					<li className="text-center">
+						<Link to="/?location=Chisinau">Chisinau </Link>
+					</li>
+				</ol>
+			</div>
 		);
 }
 
