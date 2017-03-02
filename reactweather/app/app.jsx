9e295f889c3d4badfe877e5237f83a16cdfa6cc7
lindemann18 <ashernetz@hotmail.com>
2017-03-01 20:27:00 -0700

    added sass

--- reactweather/app/app.jsx
+++ reactweather/app/app.jsx
@@ -9,7 +9,7 @@ var Examples = require("Examples");
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 $(document).foundation();
 
